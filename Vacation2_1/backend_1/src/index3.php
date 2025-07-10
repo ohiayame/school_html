81abb7d31d4fad798ae7255251c46ac227c65274
Ayame <166793391+ohiayame@users.noreply.github.com>
2025-07-10 21:15:10 +0900

    GET POST

---
+++ Vacation2_1/backend_1/src/index3.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <form action="3.php" method="post">
+        ID : <input type="text" name="id"><br>
+        PW : <input type="text" name="pw"><br>
+        <input type="submit" value="전송">
+    </form>
+</body>
+</html>
